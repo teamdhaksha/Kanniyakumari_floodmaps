3ee801ef6e78a5d3147ab8d85c7f11ea19fc36e8
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-12-20 10:52:40 +0530

    Add files via upload

---
+++ data/Towns_6.js
@@ -0,0 +1,14 @@
+var json_Towns_6 = {
+"type": "FeatureCollection",
+"name": "Towns_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Kanyakumari" }, "geometry": { "type": "Point", "coordinates": [ 77.550475539803941, 8.08583965366407, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Agasteeswaram" }, "geometry": { "type": "Point", "coordinates": [ 77.520917975238717, 8.099515461219285, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Nagercoil" }, "geometry": { "type": "Point", "coordinates": [ 77.428551009577333, 8.187569394662001, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kulasekharam" }, "geometry": { "type": "Point", "coordinates": [ 77.292347759052404, 8.365040590196767, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kaliakkavilai" }, "geometry": { "type": "Point", "coordinates": [ 77.169921333667276, 8.334022360158121, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kollamkode" }, "geometry": { "type": "Point", "coordinates": [ 77.110778172969873, 8.287363203488539, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Aralvaizozhi" }, "geometry": { "type": "Point", "coordinates": [ 77.520079784699178, 8.251939117191473, -999999.0 ] } }
+]
+}
